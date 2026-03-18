8d9312998c8bb3256dd89013c34da678da0c5bb3
Omkar <omkarmchoudhary123@gmail.com>
2026-03-18 09:47:45 +0530

    Refactor connection manager for improved readability

--- server/connection_manager.py
+++ server/connection_manager.py
@@ -5,23 +5,19 @@ lock = threading.Lock()
 
 
 def register_client(username, conn):
-
     with lock:
         if username in clients:
             return False
-
         clients[username] = conn
         return True
 
 
 def remove_client(username):
-
     with lock:
         if username in clients:
             del clients[username]
 
 
 def get_client(username):
-
     with lock:
         return clients.get(username)
